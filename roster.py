f7de33d9c3e620764eacebe1358c47a8a366d5e0
Vinny Rottger <vinrottger@gmail.com>
2023-12-04 18:31:20 -0500

    roster loop error

--- roster.py
+++ roster.py
@@ -1,4 +1,5 @@
-# https://goheels.com/sports/mens-basketball/roster
+https://goheels.com/sports/mens-basketball/roster
 
 roster = ["High", "Ryan", "Davis"]
-print(roster)
+for player in roster:  
+    print(player)
