751b5c7659001715066bad468a62eaab3df40288
Vinny Rottger <vinrottger@gmail.com>
2023-12-04 19:15:06 -0500

    removed roster

--- roster.py
+++ roster.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-roster = ["Bacot", "Davis", "Cadeau"]
-player = {"Last Name": roster,
+player = {"Last Name": ["Bacot", "Davis", "Cadeau"],
           "First Name": ["Armando", "RJ", "Elliot"],
           "height": [83, 72, 73],
           "weight": [240, 180, 180]}
