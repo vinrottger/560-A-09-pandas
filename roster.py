acc2b8cdfed9548d9b42d1c9550d3d266156eee3
Vinny Rottger <vinrottger@gmail.com>
2023-12-04 19:26:11 -0500

    bmi roster

--- roster.py
+++ roster.py
@@ -3,6 +3,13 @@ import pandas as pd
 player = {"Last Name": ["Bacot", "Davis", "Cadeau"],
           "First Name": ["Armando", "RJ", "Elliot"],
           "height": [83, 72, 73],
-          "weight": [240, 180, 180]}
+          "weight": [240, 180, 180]
+}
+
 data = pd.DataFrame(player)
+
+data["bmi"] = (data["weight"]/2.205)/((data["height"]/39.37)**2)
+
 print(data)
+
+data.to_csv("bmi.csv")
